4af89be1476d6879b3f28bcf8f7115f1a8477f84
MiguelAngel2323 <miguelangel.felipe@alu.uclm.es>
2023-04-20 10:36:04 +0200

    Interfaz Grafica

---
+++ src/Libreria.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class Libreria {
+    private JPanel mainPanel;
+    private JTextField tfTitulo;
+    private JTextField tfAutor;
+    private JTextField tfAñoPublicación;
+    private JTextArea textArea1;
+    private JTextField tfISBN;
+    private JTextField tfEditorial;
+    private JButton Comprar;
+    private JLabel Titulo;
+    private JLabel Autor;
+    private JLabel Año_Publicacion;
+    private JLabel Editorial;
+    private JLabel Genero;
+    private JLabel ISBN;
+    private JTextField tfGenero;
+    private JLabel Titulo_a_Vender;
+    private JTextField textField1;
+    private JButton Vender;
+}
